11f181f1f2dba779117e4e8002bd286fefd7b662
doloresdadd <doloresdadd@gmail.com>
2025-09-13 13:18:29 +1000

    Python Structure

---
+++ src/textnode.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class TextTypes(Enum):
+    PLAIN_TEXT = "text(plain)"
+    BOLD_TEXT = "**Bold Text**"
+    ITALIC_TEXT = "_Italic Text_"
+    CODE_TEXT = "`Code Text`"
+    LINKS = "[Link Text](http://example.com)"
+    IMAGES = "![Alt Text](http://example.com/image.jpg)"
+
+class TextNode:
+    def __init__(self, text_type: TextTypes, text: str):
+        self.text_type = text_type
+        self.text = text
+        self.url = None
+
+
+    def render(self) -> str:
+        return f"{self.text_type.value}: {self.content}"
